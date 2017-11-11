e0d08b6d6cca6db272574b15381eb56f92e28d99
Maja <manon.jacquin@gmail.com>
2017-11-11 16:10:43 -0500

    Changes syntax

--- test/unit/test-pomodoro_clock.js
+++ test/unit/test-pomodoro_clock.js
@@ -2,13 +2,13 @@ import test from "ava";
 import pomodoroClock from "../../lib/pomodoro_clock";
 
 test("converts 1 minute to 60 seconds", (t) => {
-    t.equal(pomodoroClock.convertMinToSec(1), 60);
+    t.is(pomodoroClock.convertMinToSec(1), 60);
 });
 
 test("converts 10 seconds into '0:10'", (t) => {
-    t.equal(pomodoroClock.timeToString(10), "0:10");
+    t.is(pomodoroClock.timeToString(10), "0:10");
 });
 
 test("converts 69 seconds into '1:09'", (t) => {
-    t.equal(pomodoroClock.timeToString(69), "1:09");
+    t.is(pomodoroClock.timeToString(69), "1:09");
 });
