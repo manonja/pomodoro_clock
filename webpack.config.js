8be573ea5ac3fe6237fe18ccc07b887430df5748
Maja <manon.jacquin@gmail.com>
2017-11-10 23:21:43 -0500

    Adds better gitignore and cleans code

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,23 @@
-const path = require('path')
-
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const path = require("path");
 
 module.exports = {
-  entry: './lib/entrypoint.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, 'assets'),
-    publicPath: '/',
-    port: 3000,
-    watchContentBase: true
-  },
-  devtool: 'sourcemap',
-}
+    /* Define our entry point. */
+    entry: "./lib/entrypoint.js",
+
+    /* Define our output file name. */
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+
+    /* Set up the development server. */
+    devServer: {
+        contentBase: path.resolve(__dirname, "assets"),
+        publicPath: "/",
+        port: 3000,
+        watchContentBase: true
+    },
+
+    /* Add source map so that we can use debug tools in Chrome. */
+    devtool: "sourcemap",
+};
